73c4fe6c04b3e200fab67521c0803db3c2655abc
rithwik732 <rithwik732@gmail.com>
2026-01-19 19:45:46 +0530

    Configure Next.js with custom settings

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/sports-stats-portal',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
